61942b2aa572731b6d7ce2aeb668e2775bd052c0
EvgeneOskin <eoskin@crystalnix.com>
2017-07-16 15:51:38 +0600

    Add leader election tracking and flask API to get leaders

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM python:3.6-alpine
 RUN pip install pipenv
 ADD Pipfile Pipfile.lock ./
 
-RUN pipenv install --system
+RUN apk --update --no-cache add \
+        gcc musl-dev python-dev libev-dev \
+    && pipenv install --system
 ADD main.py .
 
 CMD python -u main.py
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import os
 import datetime
 import uuid
 import logging
-from kazoo.client import KazooClient
+from time import sleep
+import gevent
+from gevent.wsgi import WSGIServer
+from flask import Flask, jsonify
 
+from kazoo.client import KazooClient, EventType
+from kazoo.exceptions import NodeExistsException, NoNodeException
 
 logging.basicConfig()
 
@@ -11,20 +16,61 @@ zoo_hosts = os.getenv('ZK_HOSTS')
 zk = KazooClient(hosts=zoo_hosts)
 zk.start()
 
-counter = zk.Counter("/connected")
-counter += 1
-
-min_nodes_number = int(os.getenv('MIN_NUMBER_OF_WORKERS'))
-barrier = zk.DoubleBarrier('/entrance', min_nodes_number)
-
-barrier.enter()
-
+identity = uuid.uuid4().hex
 lease = zk.NonBlockingLease(
     "/welcome", datetime.timedelta(minutes=1),
-    identifier="welcome " + uuid.uuid4().hex)
-
+    identifier=identity
+)
 if lease:
-    print(f"Welcome: There were ${counter.value} connections")
+    print(f"Welcome")
 
 
-barrier.leave()
+class LeaderElection(object):
+
+    def __init__(self):
+        self.identity = bytes(uuid.uuid4().hex , encoding='utf')
+        self.leader = None
+        self.election = None
+    @property
+    def is_leader(self):
+        return self.leader == self.identity
+
+    def elected(self, election):
+        try:
+            zk.create('/leader', self.identity, ephemeral=True)
+            print("I'm a leader")
+        except NodeExistsException:
+            pass
+
+    def new_election(self):
+        self.election = zk.Election("/election", self.identity)
+        self.election.run(self.elected, election)
+
+    def run_elect(self):
+        @zk.DataWatch("/leader")
+        def watch_leader(data, stat):
+            if data:
+                self.leader = data
+                if self.election:
+                    self.election.cancel()
+            else:
+                self.new_election()
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    global is_leader
+    return jsonify(
+        title='Hello',
+        leader=election.leader and str(election.leader, encoding='utf'),
+        is_leader=election.is_leader
+    )
+
+
+election = LeaderElection()
+gevent.spawn(election.run_elect)
+
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
